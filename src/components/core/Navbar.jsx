6cc31734db4bece226cb93d909744b745b43f803
pawan <pawankp550@gmail.com>
2020-05-16 21:04:15 +0530

    navbar update and animate css library added

--- src/components/core/Navbar.jsx
+++ src/components/core/Navbar.jsx
@@ -17,10 +17,10 @@ export default function Navbar() {
             </div>
 
             <div className="navbar-end">
-                <div className='navbar-shop'>
+                <div className='navbar-shop navbar-desktop-option'>
                     SHOP
                 </div>
-                <div className='navbar-user'>
+                <div className='navbar-user navbar-desktop-option'>
                     USER
                 </div>
                 <div className='navbar-cart'>
@@ -42,17 +42,17 @@ export default function Navbar() {
             </div>
 
             { menuToggle ? 
-                (<div className='navbar-mobile-menu animate__fadeIn'>
+                (<div className='navbar-mobile-menu animate__animated animate__fadeIn'>
                     <div className='navbar-close' onClick={toggleMenu}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 365.696 365.696" width="20">
                         <path d="m243.1875 182.859375 113.132812-113.132813c12.5-12.5 12.5-32.765624 0-45.246093l-15.082031-15.082031c-12.503906-12.503907-32.769531-12.503907-45.25 0l-113.128906 113.128906-113.132813-113.152344c-12.5-12.5-32.765624-12.5-45.246093 0l-15.105469 15.082031c-12.5 12.503907-12.5 32.769531 0 45.25l113.152344 113.152344-113.128906 113.128906c-12.503907 12.503907-12.503907 32.769531 0 45.25l15.082031 15.082031c12.5 12.5 32.765625 12.5 45.246093 0l113.132813-113.132812 113.128906 113.132812c12.503907 12.5 32.769531 12.5 45.25 0l15.082031-15.082031c12.5-12.503906 12.5-32.769531 0-45.25zm0 0"/>
                     </svg>
 
                     </div>
-                    <div className='navbar-shop'>
+                    <div className='navbar-shop animate__animated animate__fadeInUp'>
                         SHOP
                     </div>
-                    <div className='navbar-user'>
+                    <div className='navbar-user animate__animated animate__fadeInUp'>
                         USER
                     </div>
             </div>) : null }
